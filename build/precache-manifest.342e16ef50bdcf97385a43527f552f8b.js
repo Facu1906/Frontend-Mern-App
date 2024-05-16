e48b0b0ac9806e1d05d6f38f5f16069a84ae7380
Facundo Franco <132781893+Facu1906@users.noreply.github.com>
2024-05-16 10:36:25 -0300

    Add files via upload

---
+++ build/precache-manifest.342e16ef50bdcf97385a43527f552f8b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e084dfc670b52e83d1e4ed5cd0ca2df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "58ad68940d8e073864f1",
+    "url": "/static/css/main.5a4f7927.chunk.css"
+  },
+  {
+    "revision": "9b5933ee96f3073b3314",
+    "url": "/static/js/2.df91191e.chunk.js"
+  },
+  {
+    "revision": "58ad68940d8e073864f1",
+    "url": "/static/js/main.7b3008c2.chunk.js"
+  },
+  {
+    "revision": "c365dd7c495663b5e597",
+    "url": "/static/js/runtime-main.92d67ae5.js"
+  }
+]);
